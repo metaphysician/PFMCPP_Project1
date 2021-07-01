37c987494a935984bb47fa40185c408a3ea72bdd
Scott  Looney <scottrlooney@gmail.com>
2021-07-01 06:40:28 +0000

    updated Project 1 with nouns, actions and pseudo code functions

--- main.cpp
+++ main.cpp
@@ -14,6 +14,18 @@ I will request a change be made to one of your 10 nouns for the sole purpose of 
     Choose nouns that do things.  
     avoid choosing nouns that have things done TO them.
     If your noun consists of multiple words, use camelCaseToNameIt.
+    car
+    foot
+    cat
+    ball
+    game
+    bear
+    lawnmower
+    instrument
+    phone
+    camera
+
+
  
  2) for each of the 10 nouns, write out 3 actions it might perform, in plain english.
     a) AVOID state-checking actions, like "Car has a horn".  i.e.
@@ -32,7 +44,17 @@ I will request a change be made to one of your 10 nouns for the sole purpose of 
             other actions that would work:
                 action: consume electricity
                 action: burn out and destroy filament.
-
+       
+    car - car.movesForward() car.movesBackward() car.stops()
+    foot - foot.moves() foot.rotatesLeft() foot.stomps()
+    cat - cat.meows() cat.scratchesPost() cat.naps()
+    ball - ball.Bounces() ball.strikesObject() ball.stops()
+    game - game.starts() game.pauses() game.finishes()
+    bear - bear.attacks() bear.eatsHoney() bear.rubsTummy()
+    lawnmower - rotatesBlade() stopsBladeRotation() engagesDrivetrain()
+    instrument - instrument.tunesItself() instrument.PlaysNote() instrument.ReleasesNote()
+    phone - phone.connectsCall() phone.updatesOS() phone.applySettings()
+    camera - processImage() adjustAutoFocus() storeImageInMemory()
         
  3) write out how you'd call that action in pseudo code, in the space after the plain-english action
  
@@ -52,85 +74,88 @@ arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
+//  Noun:car
+//  action 1:car moves forward
+car.moveForward();  
 
-//  action 2:
+//  action 2:car moves backward
+car.moveBackward();
 
-//  action 3:
+//  action 3:car stops moving
+car.stop();
  
 //  2)
-//  Noun:
+//  Noun:foot  
+//  action 1:foot moving in any direction
+foot.move();
+//  action 2:rotate foot to the left
+foot.rotateLeft()
+//  action 3:stomps foot
+foot.stomp();
+//  3)  
+//  Noun: cat
+//  action 1:cat meows
+cat.meow();
+//  action 2:cat scratches post
+cat.scratchPost();
+//  action 3:cat naps
+cat.nap();
+//  4) 
+//  Noun: ball
+//  action 1:ball bounces
+ball.bounce(); 
+//  action 2:ball strikes an object
+ball.strikesObject();
+//  action 3:ball stops moving
+ball.stop()
+//  5) 
+//  Noun:game
+//  action 1:game starts
+game.start()
+//  action 2:game pauses playback
+game.pauses()
+//  action 3:game finishes
+game.finish()
+//  6) 
+//  Noun:bear   
+//  action 1:bear attacks
+bear.attack()
+//  action 2:bear eats honey
+bear.eatHoney()
+//  action 3:bear rubs its tummy
+bear.rubTummy() 
+//  7) 
+//  Noun:lawnmower   
+//  action 1:rotates blade
+lawnmower.rotateBlade();
+//  action 2:stops blade rotation
+lawnmower.stopBladeRotation();
+//  action 3:sets lawnmower in gear so it will move
+lawnmower.engageDrivetrain()
+//  8) 
+//  Noun:instrument -   
+//  action 1:instrument tunes itself
+instrument.tuneSelf()
+//  action 2:instrument plays note
+instrument.playNote()
+//  action 3:instrument releases note
+instrument.releaseNote() 
+//  9) 
+//  Noun:phone   
 //  action 1:
-
+phone.connectCall()
 //  action 2:
-
+phone.updateOS()
 //  action 3:
- 
-//  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
-//  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
- 
+phone.applySettings() 
+//  10) 
+//  Noun:camera  
+//  action 1:camera processes image received
+camera.processImage()
+//  action 2:camera adjusts autofocus
+camera.adjustAutoFocus()
+//  action 3:camera stores image in memory
+camera.storeImageInMemory()
 
 
 #endif
